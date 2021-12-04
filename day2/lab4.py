3ca6b811b5f4252616599e5c9db1d303904fd978
Nishan <nishanbasnet324@gmail.com>
2021-12-04 19:46:21 +0545

    labwork

---
+++ day2/lab4.py
@@ -0,0 +1,6 @@
+#checking which will be faster
+#taking 1 miles = 1609.344m
+distance1=4*1609.344
+speed1=(25*1609.344)/(60*60)
+time1=distance1/speed1
+totaltime1=time1+(2*10)
---
+++ day2/lab6.py
@@ -0,0 +1,5 @@
+#checking taking bus is faster or by bus
+if totaltime1<totaltime2:
+    print("by bus is faster")
+else:
+    print("by running is faster")
